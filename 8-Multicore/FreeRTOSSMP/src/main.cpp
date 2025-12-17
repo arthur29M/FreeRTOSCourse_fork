a0afb3b96e24c6a2dc0e59ec83b603a188eb5e96
arthur29M <arthur.michel@edu.esiee.fr>
2025-12-17 10:23:40 +0800

    Update main.cpp

--- 8-Multicore/FreeRTOSSMP/src/main.cpp
+++ 8-Multicore/FreeRTOSSMP/src/main.cpp
@@ -25,7 +25,7 @@
 #define TASK_PRIORITY		( tskIDLE_PRIORITY + 1UL )
 
 //LED PAD to use
-#define LED_PAD					0
+#define LED_PAD					25
 #define LED1_PAD				2
 #define LED2_PAD				3
 #define LED3_PAD				4
